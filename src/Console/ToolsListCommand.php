2f02df82dcbe85c77914b521e479c5959044b43e
abrahamghaemi <ig.ghaemi@gmail.com>
2023-03-01 16:05:18 +0330

    update desc command

--- src/Console/ToolsListCommand.php
+++ src/Console/ToolsListCommand.php
@@ -17,28 +17,20 @@ class ToolsListCommand extends Command
 {
     protected function configure()
     {
-        $this->setDescription("The operating conditions directly affect the life of an aluminum electrolytic capacitor...");
+        $this->setDescription("The operating tools");
     }
 
 	 protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $helper = $this->getHelper('question');
 
-
-
         $question = new ChoiceQuestion(
             'Please select your tools',
             ['ElectricalCapacitorLifeCalculator'],
-            '0,1'
+            '0'
         );
 
-
         $calculatorTool = $helper->ask($input, $output, $question);
-
-//        $question->setErrorMessage('Color %s is invalid.');
-//        $output->writeln('You have just selected: '.$color);
-
-
 
         if ($calculatorTool == 'ElectricalCapacitorLifeCalculator'){
             $electricalCapacitorLifeCalculator = new ElectricalCapacitorLifeCalculator;
@@ -48,33 +40,9 @@ class ToolsListCommand extends Command
             }
             $result = $electricalCapacitorLifeCalculator->calculate();
             $output->writeln('Projected Life at Operating Conditions (L2) Is : ' . $result);
-            die;
         }
 
-
-
-    	// input
-    	// validation
-    	// math
-    	// output
-
-
-        // ... put here the code to create the user
-
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 
 }
